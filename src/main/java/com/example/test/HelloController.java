7862a47d5921aebde2c7b5cb04e48f707b852a43
장하나 <onlyone728@gmail.com>
2022-01-18 17:50:48 +0900

    Spring DB 연동

--- src/main/java/com/example/test/HelloController.java
+++ src/main/java/com/example/test/HelloController.java
@@ -1,15 +1,28 @@
 package com.example.test;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class test {
+public class HelloController {
 
+	@Autowired
+	private TestDAO testDAO;
+	
 	@ResponseBody
 	@RequestMapping("/test1")
 	public String test1() {
 		return "hello world!!!";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/db_test")
+	public List<Map<String, Object>> dbTest() {
+		return testDAO.selectUser();
 	}
 }
